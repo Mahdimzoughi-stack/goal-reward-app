2532172009493ea8d863007088f73cca1ae02724
mahdi-mzoughi <mahdibenammor@gmail.com>
2025-03-02 14:13:57 +0100

    crud

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
+import {HttpClientModule} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HttpClientModule, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
+  standalone: true,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { GoalsComponent } from './components/goal/goals/goals.component';
+import { GoalFormComponent } from './components/goal/goal-form/goal-form.component';
+import { RewardsComponent } from './components/reward/rewards/rewards.component';
+import { RewardFormComponent } from './components/reward/reward-form/reward-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // Routes des Objectifs (Goals)
+  { path: 'goals', component: GoalsComponent },
+  { path: 'goals/add', component: GoalFormComponent },
+  { path: 'goals/edit/:id', component: GoalFormComponent },
+
+  // Routes des Récompenses (Rewards)
+  { path: 'rewards', component: RewardsComponent },
+  { path: 'rewards/add', component: RewardFormComponent },
+  { path: 'rewards/edit/:id', component: RewardFormComponent },
+
+  // Redirection par défaut
+  { path: '', redirectTo: '/goals', pathMatch: 'full' },
+  { path: '**', redirectTo: '/goals' } // Gestion des erreurs de routes
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
